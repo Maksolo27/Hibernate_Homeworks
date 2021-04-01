7a808ba87fa56fa5b850aa0d71606368e9145811
Maksym <71383659+Maksolo27@users.noreply.github.com>
2021-04-01 15:59:11 +0300

    Add files via upload

--- HW1/src/sample/Main.java
+++ HW1/src/sample/Main.java
@@ -1,5 +1,7 @@
 package sample;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -9,9 +11,11 @@ public class Main{
     private static final String URL = "jdbc:mysql://localhost:3306/personal?serverTimezone=UTC";
     private static final String LOGIN = "root";
     private static final String PASSWORD = "root1";
-    private static final String INSERT_NEW = "INSERT INTO emploe(name) " + " VALUE(?)";
-    private static final String DELETE = "DELETE FROM emploe WHERE name = ?  ?";
-    private static final String SELECT = "SELECT * FROM emploe";
+
+
+    private static  String INSERT_NEW = "";
+    private static  String DELETE = "";
+    private static  String SELECT = "";
 
     public static void main(String[] args) {
         Test test = new Test();
@@ -19,17 +23,63 @@ public class Main{
         Statement statement = null;
         PreparedStatement preparedStatement = null;
 
+
+        try(FileReader reader = new FileReader("C:\\Users\\maxim\\IdeaProjects\\jdbs\\HW1\\src\\INSERT_NEW"))
+        {
+            // читаем посимвольно
+            int c;
+            while((c=reader.read())!=-1){
+
+                INSERT_NEW += (char)c;
+            }
+
+        }
+
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+        try(FileReader reader = new FileReader("C:\\Users\\maxim\\IdeaProjects\\jdbs\\HW1\\src\\DELETE"))
+        {
+            // читаем посимвольно
+            int c;
+            while((c=reader.read())!=-1){
+
+                DELETE += (char)c;
+            }
+
+        }        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+        try(FileReader reader = new FileReader("C:\\Users\\maxim\\IdeaProjects\\jdbs\\HW1\\src\\SELECT"))
+        {
+            // читаем посимвольно
+            int c;
+            while((c=reader.read())!=-1){
+
+                SELECT += (char)c;
+            }
+
+        }        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+
         try {
             connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
             statement = connection.createStatement();
-/*            statement.execute("DROP table emploe");*/
-/*            statement.execute("create table emploe(" + "name varchar(100));");*/
+            statement.execute("DROP table emploe");
+            statement.execute("create table emploe(" + "name varchar(100));");
 
 
             preparedStatement = connection.prepareStatement(INSERT_NEW);
-            preparedStatement.setString(1, "name4" );
+            preparedStatement.setString(1, "name5" );
             preparedStatement.execute();
 
+            preparedStatement = connection.prepareStatement(DELETE);
+            preparedStatement.setString(1,"name76");
+            preparedStatement.execute();
             preparedStatement = connection.prepareStatement(SELECT);
 
             ResultSet resultSet = preparedStatement.executeQuery();
